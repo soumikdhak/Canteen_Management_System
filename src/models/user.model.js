22ef225fc1b9a38c8adaf7be8ae3cb44196f7c1e
soumikdhak <soumikdhak445@gmail.com>
2025-10-26 01:21:03 +0530

    feat: create user model with mongoose schema

---
+++ src/models/user.model.js
@@ -0,0 +1,10 @@
+import e from "express";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  }});
+
+  export const User = mongoose.model("User", userSchema);
