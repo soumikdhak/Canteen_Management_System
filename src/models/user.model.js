6272d6b71dc0ee12b1c90a80ed2110287c94a6a8
soumikdhak <soumikdhak445@gmail.com>
2025-10-28 00:15:57 +0530

    feat: enhance user model with password hashing, token generation methods, and indexing for email

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -36,6 +36,7 @@ const userSchema = new mongoose.Schema(
         const formattedDate = now.toISOString().replace(/[-:.TZ]/g, "").slice(0, 14);
         return `${cleanName}_${formattedDate}`;
       },
+      index:true,
     },
 
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
@@ -51,5 +52,46 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save",async function (next) {
+  if(!this.isModified("password")){
+    return next();
+  } 
+  this.password = await bcrypt.hash(this.password,10);
+  next();
+});
+
+userSchema.methods.isPasswordMatch = async function (plainPassword) {
+  return await bcrypt.compare(plainPassword, this.password);
+}
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    { _id: this._id,
+      userId: this._id, 
+      role: this.role,
+      email: this.email 
+    },
+
+    process.env.ACCESS_TOKEN_SECRET,
+
+    { 
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY 
+    }
+  );
+}
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    { _id: this._id,},
+
+    process.env.REFRESH_TOKEN_SECRET,
+
+    { 
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY 
+    }
+  );
+}
+
 
 export const User = mongoose.model("User", userSchema);
